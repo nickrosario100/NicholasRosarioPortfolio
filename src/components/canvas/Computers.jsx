b46a7ee8b0506117ef650c452a62241f020ea5f2
Nicholas Rosario <130706469+nickrosario100@users.noreply.github.com>
2023-06-22 13:51:59 -0400

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,25 +5,25 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./ship/scene.gltf");
+  const computer = useGLTF("./laptop/scene.gltf");
 
-  return (
+return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={1} groundColor="black" />
       <spotLight
-        position={[-20, 50, 10]}
+        position={[10, 0, 20]}
         angle={0.12}
         penumbra={1}
         intensity={1}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={2} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.1}
-        position={isMobile ? [0, -5, -2.2] : [0, -1, 0]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.7 : 1}
+        position={isMobile ? [0, -5, -2.2] : [0, -1.2, 0]}
+        rotation={[0.13, 1, 0.1]}
       />
     </mesh>
   );
