66ccb0da4518a212db6192b47811ef44abeef134
nickrosario100 <nick.r.rosario@gmail.com>
2023-07-20 17:16:16 -0400

    update

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import React from "react";
 
 import { BallCanvas } from "./canvas";
@@ -19,45 +17,3 @@ const Tech = () => {
 };
 
 export default SectionWrapper(Tech, "");
-=======
-import React from "react";
-
-import { BallCanvas } from "./canvas";
-import { SectionWrapper } from "../hoc";
-import { technologies } from "../constants";
-
-const Tech = () => {
-  return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default SectionWrapper(Tech, "");
->>>>>>> 1e8477bf1b345131d2d685b44a9eb565e25e8300
-=======
-import React from "react";
-
-import { BallCanvas } from "./canvas";
-import { SectionWrapper } from "../hoc";
-import { technologies } from "../constants";
-
-const Tech = () => {
-  return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default SectionWrapper(Tech, "");
->>>>>>> 1e8477bf1b345131d2d685b44a9eb565e25e8300
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
 import Hero from "./Hero";
 import Navbar from "./Navbar";
@@ -27,61 +26,3 @@ export {
   ComputersCanvas, 
   StarsCanvas
 };
-=======
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from "./Hero";
-import Navbar from "./Navbar";
-import About from "./About";
-import Tech from "./Tech";
-import Experience from "./Experience";
-import Works from "./Works";
-import Feedbacks from "./Feedbacks";
-import Contact from "./Contact";
-import CanvasLoader from "./Loader";
-
-
-export {
-  Hero,
-  Navbar,
-  About,
-  Tech,
-  Experience,
-  Works,
-  Feedbacks,
-  Contact,
-  CanvasLoader,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
-  StarsCanvas
-};
->>>>>>> 1e8477bf1b345131d2d685b44a9eb565e25e8300
-=======
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from "./Hero";
-import Navbar from "./Navbar";
-import About from "./About";
-import Tech from "./Tech";
-import Experience from "./Experience";
-import Works from "./Works";
-import Feedbacks from "./Feedbacks";
-import Contact from "./Contact";
-import CanvasLoader from "./Loader";
-
-
-export {
-  Hero,
-  Navbar,
-  About,
-  Tech,
-  Experience,
-  Works,
-  Feedbacks,
-  Contact,
-  CanvasLoader,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
-  StarsCanvas
-};
->>>>>>> 1e8477bf1b345131d2d685b44a9eb565e25e8300
